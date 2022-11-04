c15bc4bf6ef4b2ffd7432c563df7c56a74f43fe0
yinlubin <yinlubin1989@gmail.com>
2022-11-04 10:40:07 +0800

    -

--- pcaServoDriver.js
+++ pcaServoDriver.js
@@ -10,6 +10,12 @@ const main = () => {
     app.use(express.json())
     app.use(express.urlencoded({ extended: false }))
     app.use(cookieParser())
+    app.all('*', function(request, response, next) {
+        response.header('Access-Control-Allow-Origin', '*')
+        response.header('Access-Control-Allow-Headers', 'content-type')
+        response.header('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET,OPTIONS')
+        next()
+    })
     const server = http.createServer(app)
     const options = {
         i2c: i2cBus.openSync(1),
@@ -22,8 +28,12 @@ const main = () => {
             console.error("Error initializing PCA9685")
             process.exit(-1)
         }
-    
+        setInterval(() => {
+        }, 500)
         socketio(server).on('connection', (socket) => {
+            socket.on('hb', () => {
+
+            })
             socket.on('pwm', (command) => {
                 pcaDriver.setPulseLength(command.pin, command.data)
             })
